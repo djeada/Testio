824546e773ac4dbde48b41ccd01a2cf93cc28781
djeada <addjellouli1@gmail.com>
2022-01-25 22:50:54 +0100

    adding unit tests for parser

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,18 +1,73 @@
-import unittest
+from distutils.command.config import config
+from pathlib import Path
+
+import pytest
 
 from src.main import ConfigParser
 
+def test_read_single_input_config_file(tmp_path):
+    """
+    Test reading config file with single input.
+    """
+    config_path = tmp_path/Path("test_config.json")
 
-class TestParser(unittest.TestCase):
-    def test_read_config_file(self):
-        pass
+    exe_path = tmp_path/Path('program.py')
+    exe_path.touch()
 
-    def test_parse_tests(self):
-        pass
+    with open(config_path, "w") as file:
+        file.write(
+            f'''
+            {{
+                "ProgramPath": "{exe_path.name}",
+                "Timeout": 1,
+                "Test1": {{
+                    "input": ["3"],
+                    "output": ["6"]
+                }}
+            }}
+            '''
+        )
 
-    def test_validate_config_file(self):
-        pass
+    config_parser = ConfigParser(Path(config_path))
+    assert config_parser.paths == [tmp_path/Path(f'{exe_path.name}')]
+    assert config_parser.timeout == 1
+    assert len(config_parser.tests) == 1
+    assert config_parser.tests[0].input == "3"
+    assert config_parser.tests[0].output == "6"
 
 
-if __name__ == "__main__":
-    unittest.main()
+def test_read_multiple_tests_config_file(tmp_path):
+    """	
+    Test reading config file with multiple tests.
+    """
+    config_path = tmp_path/Path("test_config.json")
+
+    exe_path = tmp_path/Path('program.py')
+    exe_path.touch()
+
+    with open(config_path, "w") as file:
+        file.write(
+            f'''
+            {{
+                "ProgramPath": "{exe_path.name}",
+                "Timeout": 1,
+                "Test1": {{
+                    "input": ["3"],
+                    "output": ["6"]
+                }},
+                "Test2": {{
+                    "input": ["4"],
+                    "output": ["8"]
+                }}
+            }}
+            '''
+        )
+
+    config_parser = ConfigParser(Path(config_path))
+    assert config_parser.paths == [tmp_path/Path(f'{exe_path.name}')]
+    assert config_parser.timeout == 1
+    assert len(config_parser.tests) == 2
+    assert config_parser.tests[0].input == "3"
+    assert config_parser.tests[0].output == "6"
+    assert config_parser.tests[1].input == "4"
+    assert config_parser.tests[1].output == "8"
