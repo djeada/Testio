1be6901324a26896eadae26a46b59c677a07a2ca
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2022-02-07 00:12:15 +0100

    Create run_all.sh

---
+++ hooks/run_all.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+paths=(src tests examples)  # If you wish to check more directories, you may add them to this list.
+
+for path in "${paths[@]}"; do
+  python hooks/remove_carriage_return.py "$path"
+done
